1698caa88ec707c7717a61c0d3ffef3a7a9921da
computercrazy77 <32375789+computercrazy77@users.noreply.github.com>
2019-10-31 18:21:00 -0500

    delete old bot and add some stuff

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import coloredlogs, logging
 import yaml
+from datetime import datetime as d
 
 # Colored logs install
 l = logging.getLogger(__name__)
@@ -22,42 +23,38 @@ TOKEN = data['bot-token']
 description = """
 General purpose Discord bot.
 """
+startup = None
 
-class Robonater(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix = "robo.", description = description, activity = discord.Activity(name = "for robo.help for help", type = 3))
-
-        self.remove_command('help')
+def get_prefix(client, message):
     
-    # I HAVE NO IDEA WHAT IM DOINGGGG
+    prefixes = ['robo.', 'r.', 'Robo.', 'R.']
 
-bot = Robonater()
-#bot.remove_command('help')
+    return commands.when_mentioned_or(*prefixes)(client, message)
 
-@commands.command(name='ping', description='The ping command', aliases=['p', 'hello', 'hi'])
-async def ping(ctx):
-    await ctx.send("Pong!")
+bot = commands.Bot(                         # Create a new bot
+    command_prefix=get_prefix,              # Set the prefix
+    description='A general purpose bot.',  # Set a description for the bot
+    owner_id=224513210471022592,            # Your unique User ID
+    case_insensitive=True,                   # Make the commands case insensitive
+    activity = discord.Activity(name="for robo.help", type = 3)
+)
 
-@commands.command(name='test', description='The test command', aliases=['t'])
-async def test(ctx):
-    await ctx.send("Hello, {}".format(ctx.author.mention))
-
-@commands.command(name='help', description='The help command', aliases=['help', 'nater', 'h', 'info'])
-async def help(ctx):
-    help_commands = """
-    `{0}.help` - Help command
-    `{0}.ping` - Ping command
-    `{0}.test` - Test command
-    """.format("robo.")
-    em = discord.Embed(title = "Robo.nater Help Menu", description = help_commands)
-    await ctx.send(embed = em)
-
-bot.add_command(ping)
-bot.add_command(test)
-bot.add_command(help)
+cogs = ['cogs.meta']
 
 @bot.event
 async def on_ready():
-    l.info("Logged in as {}".format(bot.user.name))
+    global startup
 
-bot.run(TOKEN)
+    l.info(f'Logged in as {bot.user.name} - {bot.user.id}')
+
+    startup = d.now()
+
+    bot.remove_command('help')
+
+    for cog in cogs:
+        bot.load_extension(cog)
+    
+    return
+
+# Finally, login the bot
+bot.run(TOKEN, bot=True, reconnect=True)
