4c0363711ada88b1aa1de85ca76db86de44006c3
Fyssion <32375789+fyssion@users.noreply.github.com>
2020-02-19 22:20:57 -0600

    new admin cog

---
+++ cogs/admin.py
@@ -0,0 +1,123 @@
+import discord
+from discord.ext import commands
+
+from datetime import datetime as d
+import traceback
+import json
+
+
+class Admin(commands.Cog):
+    """Admin commands and features"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.log = self.bot.log
+
+        with open("active_dms.json", "r") as f:
+            self.active_dms = json.load(f)
+
+    @commands.command(
+        name="reload",
+        description="Reload an extension",
+        aliases=['load'],
+        usage="[cog]",
+        hidden=True
+    )
+    @commands.is_owner()
+    async def _reload(self, ctx, cog="all"):
+        if cog == "all":
+            msg = ""
+
+            for ext in self.bot.cogs_to_load:
+                try:
+                    self.bot.reload_extension(ext)
+                    msg += f"**:repeat: Reloaded** `{ext}`\n\n"
+                    self.log.info(f"Extension '{cog.lower()}' successfully reloaded.")
+
+                except Exception as e:
+                    traceback_data = ''.join(traceback.format_exception(type(e), e, e.__traceback__, 1))
+                    msg += (f"**:warning: Extension `{ext}` not loaded.**\n"
+                            f"```py\n{traceback_data}```\n\n")
+                    self.log.warning(f"Extension 'cogs.{cog.lower()}' not loaded.\n"
+                                     f"{traceback_data}")
+            return await ctx.send(msg)
+
+        try:
+            self.bot.reload_extension(cog.lower())
+            await ctx.send(f"**:repeat: Reloaded** `{cog.lower()}`")
+            self.log.info(f"Extension '{cog.lower()}' successfully reloaded.")
+        except Exception as e:
+            traceback_data = ''.join(traceback.format_exception(type(e), e, e.__traceback__, 1))
+            await ctx.send(f"**:warning: Extension `{cog.lower()}` not loaded.**\n```py\n{traceback_data}```")
+            self.log.warning(f"Extension 'cogs.{cog.lower()}' not loaded.\n{traceback_data}")
+
+    @commands.group(name="error", hidden=True, aliases=["e"])
+    @commands.is_owner()
+    async def _error(self, ctx):
+        pass
+
+    @_error.command()
+    async def previous(self, ctx):
+        if not self.bot.previous_error:
+            return await ctx.send("No previous error cached.")
+        e = self.bot.previous_error
+        error = ''.join(traceback.format_exception(type(e), e, e.__traceback__, 1))
+        await ctx.send(f"```py\n{error}```")
+
+    @commands.command(
+        name="logout",
+        description="Logs out and shuts down bot",
+        hidden=True
+    )
+    @commands.is_owner()
+    async def logout_command(self, ctx):
+        self.log.info("Logging out of Discord.")
+        await ctx.send("Logging out :wave:")
+        await self.bot.session.close()
+        await self.bot.logout()
+
+    @commands.group(description="DMs with the bot", aliases=["dms"],
+                    invoke_without_command=True)
+    @commands.is_owner()
+    async def dm(self, ctx):
+        await self.all_dms(ctx)
+
+    @dm.command(name="all", description="View all current DMs.")
+    @commands.is_owner()
+    async def all_dms(self, ctx):
+        if not self.active_dms:
+            return await ctx.send("No active DMs.")
+        dms = "Current active DMs"
+        for dm in self.active_dms:
+            pass
+        await ctx.send(dms)
+
+    @dm.command(description="Create a new DM with a user.", aliases=["new"])
+    @commands.is_owner()
+    async def create(self, ctx, member: discord.Member):
+        pass
+
+    @dm.command(description="Remove a DM with a user.", aliases=["delete", "stop"])
+    @commands.is_owner()
+    async def remove(self, ctx, member: discord.Member = None):
+        pass
+
+    @dm.command(description="Toggle broadcasting DMs")
+    @commands.is_owner()
+    async def broadcast(self, ctx, state: bool):
+        pass
+
+    @commands.Cog.listener("on_message")
+    async def dm_listener(self, message):
+        if isinstance(message.channel, discord.DMChannel) and not message.author.bot:
+            channel = self.bot.get_channel(679841169248747696)
+            em = discord.Embed(description=message.clean_content,
+                               color=discord.Color.blue(), timestamp=d.utcnow())
+            em.set_author(name=message.author, icon_url=message.author.avatar_url)
+            em.set_footer(text="Incoming DM")
+            return await channel.send(embed=em)
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
