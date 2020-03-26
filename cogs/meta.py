91aed5f57c5b122c5eb3bf26062fadc0a3d97bbc
Fyssion <32375789+fyssion@users.noreply.github.com>
2020-03-26 09:27:17 -0500

    source command
    stolen from R. Danny lol

--- cogs/meta.py
+++ cogs/meta.py
@@ -10,6 +10,7 @@ import pathlib
 import json
 import sys
 import asyncio
+import inspect
 
 from .utils.utils import wait_for_deletion
 from .utils import db
@@ -585,6 +586,45 @@ class Meta(commands.Cog, name=":gear: Meta"):
             await bot_message.edit(content="**Canceled**")
             await bot_message.remove_reaction("✅", ctx.guild.me)
             await bot_message.remove_reaction("❌", ctx.guild.me)
+
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command.
+        To display the source code of a subcommand you can separate it by
+        periods, e.g. tag.create for the create subcommand of the tag command
+        or by spaces.
+        """
+        source_url = "https://github.com/Fyssion/Clam"
+        branch = "master"
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace(".", " "))
+            if obj is None:
+                return await ctx.send("Could not find command.")
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace("\\", "/")
+        else:
+            location = module.replace(".", "/") + ".py"
+            source_url = "https://github.com/Rapptz/discord.py"
+            branch = "master"
+
+        final_url = f"<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>"
+        await ctx.send(final_url)
 
     @commands.command(name="backup_reload")
     @commands.is_owner()
