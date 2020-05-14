d39496ee8be61a9c8aa21a00d9a6d8ad18484154
Fyssion <32375789+fyssion@users.noreply.github.com>
2020-05-13 20:43:34 -0500

    Add debug logging to file

--- bot.py
+++ bot.py
@@ -12,6 +12,15 @@ import json
 from cogs.utils import backup
 
 
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="clam.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
+
+
 class Context(commands.Context):
     @property
     def guild_prefix(self):
