7e53408b068ff828eac24e2ad5782f85006dce27
Fyssion <fyssioncodes@gmail.com>
2020-06-08 14:22:13 -0500

    Move docs to Internet

--- cogs/internet.py
+++ cogs/internet.py
@@ -5,8 +5,12 @@ from datetime import datetime
 import dateparser
 import aiohttp
 import importlib
+import re
+import os
+import ast
 
-from .utils import aiopypi, aioxkcd
+from .utils import aiopypi, aioxkcd, fuzzy
+from .utils.utils import SphinxObjectFileReader
 
 
 class Internet(commands.Cog):
@@ -329,6 +333,169 @@ class Internet(commands.Cog):
             text=f"Comic published {comic.date_str}", icon_url=self.bot.user.avatar_url
         )
         await ctx.send(embed=em)
+
+    def parse_object_inv(self, stream, url):
+        # key: URL
+        # n.b.: key doesn't have `discord` or `discord.ext.commands` namespaces
+        result = {}
+
+        # first line is version info
+        inv_version = stream.readline().rstrip()
+
+        if inv_version != "# Sphinx inventory version 2":
+            raise RuntimeError("Invalid objects.inv file version.")
+
+        # next line is "# Project: <name>"
+        # then after that is "# Version: <version>"
+        projname = stream.readline().rstrip()[11:]
+        version = stream.readline().rstrip()[11:]
+
+        # next line says if it's a zlib header
+        line = stream.readline()
+        if "zlib" not in line:
+            raise RuntimeError("Invalid objects.inv file, not z-lib compatible.")
+
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(r"(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)")
+        for line in stream.read_compressed_lines():
+            match = entry_regex.match(line.rstrip())
+            if not match:
+                continue
+
+            name, directive, prio, location, dispname = match.groups()
+            domain, _, subdirective = directive.partition(":")
+            if directive == "py:module" and name in result:
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
+                # two inventory entries are created
+                # for Python modules, and the first
+                # one is correct
+                continue
+
+            # Most documentation pages have a label
+            if directive == "std:doc":
+                subdirective = "label"
+
+            if location.endswith("$"):
+                location = location[:-1] + name
+
+            key = name if dispname == "-" else dispname
+            prefix = f"{subdirective}:" if domain == "std" else ""
+
+            if projname == "discord.py":
+                key = key.replace("discord.ext.commands.", "").replace("discord.", "")
+
+            result[f"{prefix}{key}"] = os.path.join(url, location)
+
+        return result
+
+    async def build_rtfm_lookup_table(self, page_types):
+        cache = {}
+        for key, page in page_types.items():
+            sub = cache[key] = {}
+            async with self.bot.session.get(page + "/objects.inv") as resp:
+                if resp.status != 200:
+                    raise RuntimeError(
+                        "Cannot build rtfm lookup table, try again later."
+                    )
+
+                stream = SphinxObjectFileReader(await resp.read())
+                cache[key] = self.parse_object_inv(stream, page)
+
+        self._rtfm_cache = cache
+
+    async def do_rtfm(self, ctx, key, obj):
+        page_types = {
+            "latest": "https://discordpy.readthedocs.io/en/latest",
+            "latest-jp": "https://discordpy.readthedocs.io/ja/latest",
+            "python": "https://docs.python.org/3",
+            "python-jp": "https://docs.python.org/ja/3",
+        }
+
+        if obj is None:
+            await ctx.send(page_types[key])
+            return
+
+        if not hasattr(self, "_rtfm_cache"):
+            await ctx.trigger_typing()
+            # em = discord.Embed(colour = discord.Colour.blurple())
+            # em.add_field(name = "\u200b", value = ":mag: `Searching the docs...`")
+            # bot_msg = await ctx.send(embed = em)
+            await self.build_rtfm_lookup_table(page_types)
+
+        obj = re.sub(r"^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)", r"\1", obj)
+
+        if key.startswith("latest"):
+            # point the abc.Messageable types properly:
+            q = obj.lower()
+            for name in dir(discord.abc.Messageable):
+                if name[0] == "_":
+                    continue
+                if q == name:
+                    obj = f"abc.Messageable.{name}"
+                    break
+
+        cache = list(self._rtfm_cache[key].items())
+
+        def transform(tup):
+            return tup[0]
+
+        matches = fuzzy.finder(obj, cache, key=lambda t: t[0], lazy=False)[:7]
+
+        em = discord.Embed(colour=discord.Colour.blurple())
+        if len(matches) == 0:
+            return await ctx.send("Could not find anything. Sorry.")
+        em.add_field(
+            name=f"`Results for '{obj}'`",
+            value="\n".join(f"[`{key}`]({url})" for key, url in matches),
+        )
+        # em.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches)
+        # await bot_msg.edit(embed = em)
+        await ctx.send(embed=em)
+
+    def transform_rtfm_language_key(self, ctx, prefix):
+        if ctx.guild is not None:
+            #                             日本語 category
+            if ctx.channel.category_id == 490287576670928914:
+                return prefix + "-jp"
+            #                    d.py unofficial JP
+            elif ctx.guild.id == 463986890190749698:
+                return prefix + "-jp"
+        return prefix
+
+    @commands.group(
+        aliases=["rtfm", "rtfd"],
+        invoke_without_command=True,
+        description="Gives you a documentation link for a discord.py entity.",
+    )
+    async def docs(self, ctx, *, obj: str = None):
+        """Gives you a documentation link for a discord.py entity.
+        Events, objects, and functions are all supported through a
+        a cruddy fuzzy algorithm.
+        """
+        key = self.transform_rtfm_language_key(ctx, "latest")
+        await self.do_rtfm(ctx, key, obj)
+
+    @docs.command(name="python", aliases=["py"])
+    async def docs_python(self, ctx, *, obj: str = None):
+        """Gives you a documentation link for a Python entity."""
+        key = self.transform_rtfm_language_key(ctx, "python")
+        await self.do_rtfm(ctx, key, obj)
+
+    def insert_returns(self, body):
+        # insert return stmt if the last expression is a expression statement
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+
+        # for if statements, we insert returns into the body and the orelse
+        if isinstance(body[-1], ast.If):
+            insert_returns(body[-1].body)
+            insert_returns(body[-1].orelse)
+
+        # for with blocks, again we insert returns into the body
+        if isinstance(body[-1], ast.With):
+            insert_returns(body[-1].body)
 
 
 def setup(bot):
