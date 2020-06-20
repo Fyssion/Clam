600301ff662e2572cc24d7b3c1f3037ebd3e5e08
Fyssion <fyssioncodes@gmail.com>
2020-06-20 12:49:44 -0500

    sql commands from rdanny

--- cogs/admin.py
+++ cogs/admin.py
@@ -5,8 +5,12 @@ from datetime import datetime as d
 import traceback
 import psutil
 import typing
+import time
+from jishaku.codeblocks import codeblock_converter
 
+from .utils.utils import TabularData
 from .utils.menus import MenuPages
+from .utils.human_time import plural
 from .utils import colors
 
 
@@ -101,6 +105,71 @@ class Admin(commands.Cog):
         if not await commands.is_owner().predicate(ctx):
             raise commands.NotOwner("You do not own this bot.")
         return True
+
+    # https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L353-L419
+    @commands.command(hidden=True)
+    async def sql(self, ctx, *, code: codeblock_converter):
+        """Run some SQL."""
+        # the imports are here because I imagine some people would want to use
+        # this cog as a base for their other cog, and since this one is kinda
+        # odd and unnecessary for most people, I will make it easy to remove
+        # for those people.
+        lang, query = code
+
+        is_multistatement = query.count(';') > 1
+        if is_multistatement:
+            # fetch does not support multiple statements
+            strategy = ctx.db.execute
+        else:
+            strategy = ctx.db.fetch
+
+        try:
+            start = time.perf_counter()
+            results = await strategy(query)
+            dt = (time.perf_counter() - start) * 1000.0
+        except Exception:
+            return await ctx.send(f'```py\n{traceback.format_exc()}\n```')
+
+        rows = len(results)
+        if is_multistatement or rows == 0:
+            return await ctx.send(f'`{dt:.2f}ms: {results}`')
+
+        headers = list(results[0].keys())
+        table = TabularData()
+        table.set_columns(headers)
+        table.add_rows(list(r.values()) for r in results)
+        render = table.render()
+
+        fmt = f'```\n{render}\n```\n*Returned {plural(rows):row} in {dt:.2f}ms*'
+        if len(fmt) > 2000:
+            fp = io.BytesIO(fmt.encode('utf-8'))
+            await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
+        else:
+            await ctx.send(fmt)
+
+    @commands.command(hidden=True)
+    async def sql_table(self, ctx, *, table_name: str):
+        """Runs a query describing the table schema."""
+
+        query = """SELECT column_name, data_type, column_default, is_nullable
+                   FROM INFORMATION_SCHEMA.COLUMNS
+                   WHERE table_name = $1
+                """
+
+        results = await ctx.db.fetch(query, table_name)
+
+        headers = list(results[0].keys())
+        table = TabularData()
+        table.set_columns(headers)
+        table.add_rows(list(r.values()) for r in results)
+        render = table.render()
+
+        fmt = f'```\n{render}\n```'
+        if len(fmt) > 2000:
+            fp = io.BytesIO(fmt.encode('utf-8'))
+            await ctx.send('Too many results...', file=discord.File(fp, 'results.txt'))
+        else:
+            await ctx.send(fmt)
 
     @commands.group(
         description="View the blacklist", hidden=True, invoke_without_command=True
