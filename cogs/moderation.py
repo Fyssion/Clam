9fa78b8856ac902c87e53687ec55f095ae11dfff
Fyssion <fyssioncodes@gmail.com>
2020-11-02 08:56:29 -0600

    Get rid of old purge command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1391,26 +1391,6 @@ class Moderation(commands.Cog):
 
         await bot_message.clear_reactions()
 
-    @commands.command(
-        name="purge",
-        description="Purge messages in a channel",
-    )
-    @checks.has_permissions(manage_messages=True)
-    @commands.bot_has_permissions(manage_messages=True)
-    async def purge_command(self, ctx, amount: int = None):
-        async def warn_them(search):
-            return await ctx.confirm(f"This action might delete up to {search} messages. Continue?")
-
-        if amount is None:
-            amount = 100
-            if not await warn_them(amount):
-                return await ctx.send("Aborted")
-
-        deleted = await ctx.channel.purge(limit=amount + 1)
-        return await ctx.channel.send(
-            f"{ctx.tick(True)} Deleted {len(deleted)} message(s)", delete_after=5
-        )
-
     # sourced from Rapptz/RoboDanny
     # https://github.com/Rapptz/RoboDanny/blob/7cd472ca021e9e166959e91a7ff64036474ea46c/cogs/mod.py#L659-L704
     async def _basic_cleanup_strategy(self, ctx, search):
