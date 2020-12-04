158a4c7068443178e2db87752fb9f0f2ff0bd2fb
Fyssion <fyssioncodes@gmail.com>
2020-12-04 09:58:30 -0600

    Fix mistake in reload all command

--- cogs/admin.py
+++ cogs/admin.py
@@ -481,7 +481,7 @@ class Admin(commands.Cog):
                 try:
                     actual_module = sys.modules[module]
                 except KeyError:
-                    statuses.append(f"\N{SLEEPING SYMBOL} {module}")
+                    statuses.append(("\N{SLEEPING SYMBOL}", module))
                 else:
                     try:
                         importlib.reload(actual_module)
