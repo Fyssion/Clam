06bbf91ed83e6ae3a1301d91a14a18726a7e67ee
Fyssion <fyssioncodes@gmail.com>
2020-12-15 15:59:10 -0600

    Better !g command
    Rewritten ?g command from RoboDanny

--- cogs/internet.py
+++ cogs/internet.py
@@ -14,7 +14,10 @@ import json
 import functools
 import io
 import async_cse
+import traceback
+from lxml import etree
 import mediawiki
+from urllib.parse import quote as uriquote
 from PIL import Image
 from bs4 import BeautifulSoup
 
@@ -162,29 +165,12 @@ class Internet(commands.Cog):
 
         description = f"{summary}\n\n[Read more]({page.url})"
 
-        em = discord.Embed(title=title, description=description, color=color, url=page.url)
+        em = discord.Embed(
+            title=title, description=description, color=color, url=page.url
+        )
         em.set_author(name=f"Wikipedia result for '{query}'")
 
         await ctx.send(embed=em)
-
-    @commands.command(aliases=["q"])
-    @commands.cooldown(5, 30, commands.BucketType.user)
-    async def question(self, ctx, *, question):
-        """Ask the bot for information
-
-        The bot will query wolfram alpha and then wikipedia
-        if wolfram alpha returns no results.
-        """
-        partial = functools.partial(self.bot.wolfram.query, question)
-
-        async with ctx.typing():
-            result = await self.bot.loop.run_in_executor(None, partial)
-
-        if result["@success"] != "true":
-            return await self.search_wikipedia(ctx, question, color=0xDD1100)
-
-        menu = MenuPages(WolframResultSource(result, question), clear_reactions_after=True)
-        await menu.start(ctx)
 
     @commands.command(aliases=["wiki"])
     @commands.cooldown(5, 30, commands.BucketType.user)
@@ -209,11 +195,353 @@ class Internet(commands.Cog):
         menu = MenuPages(WolframResultSource(result, query), clear_reactions_after=True)
         await menu.start(ctx)
 
+    # modified google command from Rapptz
+    def parse_google_card(self, node):
+        e = discord.Embed(colour=0x4285F4)
+
+        # check if it's a calculator card:
+        calculator = node.find(".//div[@class='tyYmIf']")
+        if calculator is not None:
+            e.title = "Calculator"
+            result = node.find(".//span[@class='qv3Wpe']")
+            if result is not None:
+                result = " ".join((calculator.text, result.text.strip()))
+            else:
+                result = calculator.text + " ???"
+            e.description = result
+            return e
+
+        # check for unit conversion card
+
+        # unit_conversions = node.xpath(".//input[contains(@class, '_eif') and @value]")
+        unit_conversions = node.xpath(".//input[contains(@class, 'vXQmIe') and @value]")
+        if len(unit_conversions) == 2:
+            e.title = "Unit Conversion"
+
+            # the <input> contains our values, first value = second value essentially.
+            # these <input> also have siblings with <select> and <option selected=1>
+            # that denote what units we're using
+
+            # We will get 2 <option selected="1"> nodes by traversing the parent
+            # The first unit being converted (e.g. Miles)
+            # The second unit being converted (e.g. Feet)
+
+            xpath = etree.XPath("parent::div/select/option[@selected='1']/text()")
+            try:
+                first_node = unit_conversions[0]
+                first_unit = xpath(first_node)[0]
+                first_value = float(first_node.get("value"))
+                second_node = unit_conversions[1]
+                second_unit = xpath(second_node)[0]
+                second_value = float(second_node.get("value"))
+                e.description = " ".join(
+                    (str(first_value), first_unit, "=", str(second_value), second_unit)
+                )
+            except Exception:
+                return None
+            else:
+                return e
+
+        # check for currency conversion card
+        if "obcontainer" in node.get("class", ""):
+            currency_selectors = node.xpath(".//div[@class='J8cLR']")
+            if len(currency_selectors) == 2:
+                e.title = "Currency Conversion"
+                # Inside this <div> is a <select> with <option selected="1"> nodes
+                # just like the unit conversion card.
+
+                first_node = currency_selectors[0]
+                first_currency = first_node.find("./select/option[@selected='1']")
+
+                second_node = currency_selectors[1]
+                second_currency = second_node.find("./select/option[@selected='1']")
+
+                # The parent of the nodes have a <input class='vk_gy vk_sh ccw_data' value=...>
+                xpath = etree.XPath(
+                    "parent::td/parent::tr/td/input[contains(@class, 'vk_gy vk_sh Hg3mWc')]"
+                )
+                try:
+                    first_value = float(xpath(first_node)[0].get("value"))
+                    second_value = float(xpath(second_node)[0].get("value"))
+
+                    values = (
+                        str(first_value),
+                        first_currency.text,
+                        f'({first_currency.get("value")})',
+                        "=",
+                        str(second_value),
+                        second_currency.text,
+                        f'({second_currency.get("value")})',
+                    )
+                    e.description = " ".join(values)
+                except Exception:
+                    return None
+                else:
+                    return e
+
+        # check for generic information card
+        info = node.find(".//div[@class='ifM9O']")
+        if info is not None:
+            try:
+                e.title = "".join(info.itertext()).strip()
+                actual_information = info.xpath(
+                    "parent::div/parent::div//div[@class='_XWk' or @class='Z0LcW XcVN5d AZCkJd' or contains(@class, 'kpd-ans')]"
+                )[0]
+                e.description = "".join(actual_information.itertext()).strip()
+            except Exception:
+                return None
+            else:
+                return e
+
+        # check for translation card
+        translation = node.find(".//div[@id='tw-ob']")
+        if translation is not None:
+            src_text = translation.find(".//pre[@id='tw-source-text']/span")
+            dest_text = translation.find(".//pre[@id='tw-target-text']/span")
+
+            # the language is in another div
+            langs = node.find(".//div[@id='tw-plp']")
+            if translation is None:
+                return None
+
+            src_lang = langs.find(".//div[@id='tw-sl']/span[@class='source-language']")
+            dest_lang = langs.find(".//div[@id='tw-tl']/span[@class='target-language']")
+
+            # TODO: bilingual dictionary nonsense?
+
+            e.title = "Translation"
+            try:
+                e.add_field(name=src_lang.text, value=src_text.text, inline=True)
+                e.add_field(name=dest_lang.text, value=dest_text.text, inline=True)
+            except Exception:
+                return None
+            else:
+                return e
+
+        # check for "time in" card
+        time = node.find("./div[@class='vk_c vk_gy vk_sh card-section sL6Rbf R36Kq']")
+        # print("time", time)
+        if time is not None:
+            try:
+                e.title = node.find("span").text
+                e.description = f'{time.text}\n{"".join(time.itertext()).strip()}'
+            except Exception:
+                return None
+            else:
+                return e
+
+        # time in has an alternative form without spans
+        time = node.find("./div[@class='vk_bk vk_ans _nEd']")
+        if time is not None:
+            converted = "".join(time.itertext()).strip()
+            try:
+                # remove the in-between text
+                parent = time.getparent()
+                parent.remove(time)
+                original = "".join(parent.itertext()).strip()
+                e.title = "Time Conversion"
+                e.description = f"{original}...\n{converted}"
+            except Exception:
+                return None
+            else:
+                return e
+
+        # check for definition card
+        words = node.xpath(".//span[@data-dobid='hdw']")
+        if words:
+            lex = etree.XPath(".//div[@class='pgRvse vdBwhd']/i/span")
+
+            # this one is derived if we were based on the position from lex
+            xpath = etree.XPath(
+                "../../../../../ol[@class='eQJLDd']//"
+                "div[not(@class and @class='lr_dct_sf_subsen')]/"
+                "div[@class='QIclbb']/div[@data-dobid='dfn']/span"
+            )
+            for word in words:
+                # we must go three parents up to get the root node
+                root = word.getparent().getparent().getparent().getparent()
+
+                pronunciation = root.find(".//span[@class='XpoqFe']/span")
+                if pronunciation is None:
+                    continue
+
+                lexical_category = lex(root)
+                definitions = xpath(root)
+
+                for category in lexical_category:
+                    definitions = xpath(category)
+                    try:
+                        descrip = [f"*{category.text}*"]
+                        for index, value in enumerate(definitions, 1):
+                            descrip.append(f"{index}. {value.text}")
+
+                        e.add_field(
+                            name=f"{word.text} /{pronunciation.text}/",
+                            value="\n".join(descrip),
+                        )
+                    except Exception:
+                        continue
+
+            return e
+
+        # check for weather card
+        location = node.find("./div[@id='wob_loc']")
+        if location is None:
+            return None
+
+        # these units should be metric
+
+        date = node.find("./div[@id='wob_dts']")
+
+        # <img alt="category here" src="cool image">
+        category = node.find(".//img[@id='wob_tci']")
+
+        xpath = etree.XPath(
+            ".//div[@id='wob_d']//div[contains(@class, 'vk_bk')]//span[@class='wob_t']"
+        )
+        temperatures = xpath(node)
+
+        misc_info_node = node.find(".//div[@class='vk_gy vk_sh wob-dtl']")
+
+        if misc_info_node is None:
+            return None
+
+        precipitation = misc_info_node.find("./div/span[@id='wob_pp']")
+        humidity = misc_info_node.find("./div/span[@id='wob_hm']")
+        wind = misc_info_node.find("./div/span/span[@id='wob_tws']")
+
+        try:
+            e.title = "Weather for " + location.text.strip()
+            e.description = f'*{category.get("alt")}*'
+            e.set_thumbnail(url="https:" + category.get("src"))
+
+            if len(temperatures) == 4:
+                first_unit = temperatures[0].text + temperatures[2].text
+                second_unit = temperatures[1].text + temperatures[3].text
+                units = f"{first_unit} | {second_unit}"
+            else:
+                units = "Unknown"
+
+            e.add_field(name="Temperature", value=units, inline=False)
+
+            if precipitation is not None:
+                e.add_field(name="Precipitation", value=precipitation.text)
+
+            if humidity is not None:
+                e.add_field(name="Humidity", value=humidity.text)
+
+            if wind is not None:
+                e.add_field(name="Wind", value=wind.text)
+        except Exception:
+            return None
+
+        return e
+
+    async def get_google_entries(self, query):
+        url = f"https://www.google.com/search?q={uriquote(query)}"
+        params = {"safe": "on", "lr": "lang_en", "hl": "en"}
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) Gecko/20100101 Firefox/53.0"
+        }
+
+        # list of URLs and title tuples
+        entries = []
+
+        # the result of a google card, an embed
+        card = None
+
+        async with self.bot.session.get(url, params=params, headers=headers) as resp:
+            if resp.status != 200:
+                self.log.info(
+                    "Google failed to respond with %s status code.", resp.status
+                )
+                raise RuntimeError("Google has failed to respond.")
+
+            root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+            # for bad in root.xpath("//style"):
+            #     bad.getparent().remove(bad)
+
+            # for bad in root.xpath("//script"):
+            #     bad.getparent().remove(bad)
+
+            # with open("google.html", "w", encoding="utf-8") as f:
+            #     f.write(etree.tostring(root, pretty_print=True).decode("utf-8"))
+
+            """
+            Tree looks like this.. sort of..
+            <div class="rc">
+                <h3 class="r">
+                    <a href="url here">title here</a>
+                </h3>
+            </div>
+            """
+
+            card_node = root.xpath(
+                ".//div[@id='rso']/div[@class='ULSxyf' or @class='hlcw0c']//"
+                "div[contains(@class, 'vk_c') or @class='card-section' or @class='g mnr-c g-blk' "
+                "or @class='kp-blk' or @class='RQXSBc' or @class='g obcontainer' or @class='YQaNob']"
+            )
+
+            # print("card node", card_node)
+
+            if card_node is None or len(card_node) == 0:
+                card = None
+            else:
+                card = self.parse_google_card(card_node[0])
+
+            search_results = root.findall(".//div[@class='rc']")
+            # print(len(search_results))
+            for node in search_results:
+                link = node.find("./div[@class='yuRUbf']/a")
+                if link is not None:
+                    # print(etree.tostring(link, pretty_print=True).decode())
+                    span = link.find("./h3/div[@class='ellip']/span")
+                    text = span.text if span is not None else link.text
+                    entries.append((link.get("href"), text))
+
+        return card, entries
+
+    @commands.command(aliases=["google"])
+    async def g(self, ctx, *, query):
+        """Searches google and gives you the top result."""
+        await ctx.trigger_typing()
+        try:
+            card, entries = await self.get_google_entries(query)
+        except RuntimeError as e:
+            await ctx.send(str(e))
+        else:
+            if card:
+                value = "\n".join(
+                    f'[{title}]({url.replace(")", "%29")})'
+                    for url, title in entries[:3]
+                )
+                if value:
+                    card.add_field(name="Search Results", value=value, inline=False)
+                return await ctx.send(embed=card)
+
+            if len(entries) == 0:
+                return await ctx.send("No results found... sorry.")
+
+            next_two = [x[0] for x in entries[1:3]]
+            first_entry = entries[0][0]
+            if first_entry[-1] == ")":
+                first_entry = first_entry[:-1] + "%29"
+
+            if next_two:
+                formatted = "\n".join(f"<{x}>" for x in next_two)
+                msg = f"{first_entry}\n\n**See also:**\n{formatted}"
+            else:
+                msg = first_entry
+
+            await ctx.send(msg)
+
     @commands.command(
-        description="Preform a google search and display the results", aliases=["g"]
+        description="Preform a google search via the API and display the results", aliases=["gapi"]
     )
     @commands.cooldown(5, 30, commands.BucketType.user)
-    async def google(self, ctx, *, query):
+    async def googleapi(self, ctx, *, query):
         google_client = self.bot.google_client
 
         try:
@@ -224,6 +552,27 @@ class Internet(commands.Cog):
 
         pages = MenuPages(GoogleResultPages(results, query), clear_reactions_after=True)
         await pages.start(ctx)
+
+    @commands.command(aliases=["question"])
+    @commands.cooldown(5, 30, commands.BucketType.user)
+    async def q(self, ctx, *, question):
+        """Ask the bot for information
+
+        The bot will query wolfram alpha and then wikipedia
+        if wolfram alpha returns no results.
+        """
+        partial = functools.partial(self.bot.wolfram.query, question)
+
+        async with ctx.typing():
+            result = await self.bot.loop.run_in_executor(None, partial)
+
+        if result["@success"] != "true":
+            return await ctx.invoke(self.g, query=question)
+
+        menu = MenuPages(
+            WolframResultSource(result, question), clear_reactions_after=True
+        )
+        await menu.start(ctx)
 
     @commands.group(
         description="Fetch a PyPI package.",
